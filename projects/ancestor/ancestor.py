e927d626b2ccb1a283229ef3453270c15224f269
josiah <josiahdamiwilliams@gmail.com>
2020-07-15 14:05:59 +0100

    Ancestor.py from lecture class

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,49 +1,63 @@
-from util import Queue
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
 
 
+class Graph:
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        self.vertices = {}
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist!")
 
-
+    '''
+       10
+     /
+    1   2   4  11
+     \ /   / \ /
+      3   5   8
+       \ / \   \
+        6   7   9
+    '''
 def earliest_ancestor(ancestors, starting_node):
-    pass
-    # start with making a Queue
+    # Build the graph
+    graph = Graph()
+    for pair in ancestors:
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+        # Build edges in reverse
+        graph.add_edge(pair[1], pair[0])
+    # Do a BFS (storing the path)
     q = Queue()
-    # Enqueue list with starting node
-    q .enqueue([starting_node])
-    # start with empty list of ancestors
-    longest_path = []
-    # make empty visited set
-    visited = set()
-
-    # while queue is not empty
+    q.enqueue([starting_node])
+    max_path_len = 1
+    earliest_ancestor = -1
     while q.size() > 0:
-        # dequeue list in
-        path  =  q.dequeue()
-        # look at the parent(1st element) in dequque' d tuple
-        v = path [-1]
-        
-        # see if parent has been visited
-        if v not in visited:
-            # mark as visited
-            visited.add(v)
-
-
-        # check if one is longer than the rest
-        if (len(path) > len(longest_path)):
-            longest_path = path
-
-        for i in range(len(ancestors)):
-            if ancestors[i][1] is v:
-                new_path = path.copy()
-                # print(new_path)
-                new_path.append(ancestors[i][0])
-                q.enqueue(new_path)
-        print('longest_path', longest_path)
-
-
-        earliest = longest_path.pop()
-
-        if earliest is starting_node:
-            earliest = -1
-        return earliest
+        path = q.dequeue()
+        v = path[-1]
+        # If the path is longer or equal and the value is smaller, or if the path is longer)
+        if (len(path) >= max_path_len and v < earliest_ancestor) or (len(path) > max_path_len):
+            earliest_ancestor = v
+            max_path_len = len(path)
+        for neighbor in graph.vertices[v]:
+            path_copy = list(path)
+            path_copy.append(neighbor)
+            q.enqueue(path_copy)
+    return earliest_ancestor
 
            
